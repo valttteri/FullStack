bfc8498ff15cd226006b810fb9d3b5816da770ae
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-02-15 09:59:30 +0200

    blogilista step2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
