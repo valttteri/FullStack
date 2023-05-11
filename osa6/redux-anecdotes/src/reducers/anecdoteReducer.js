1de1b386918f1f0c4723ea4c03cac914a3f8a3e5
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-05-11 16:55:34 +0300

    git just died, testing

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -6,8 +6,8 @@ const anecdoteSlice = createSlice({
   initialState: [],
   reducers: {
     addVote(state, action) {
-      //console.log(JSON.parse(JSON.stringify(state)))
-      //console.log(JSON.parse(JSON.stringify(action)))
+      console.log(JSON.parse(JSON.stringify(state)))
+      console.log(JSON.parse(JSON.stringify(action)))
       
       console.log('payload: ', action.payload)
       const id = action.payload.id
@@ -61,23 +61,6 @@ export const voteAnecdote = (id, content, votes) => {
     dispatch(addVote(data))
   }
 }
-
-/*
-
-voteAnecdote(state, action) {
-      console.log(JSON.parse(JSON.stringify(state)))
-      console.log(JSON.parse(JSON.stringify(action)))
-
-      const id = action.payload
-      const votedAnecdote = state.find(a => a.id === id)
-      const updatedAnecdote = {
-        ...votedAnecdote,
-        votes: votedAnecdote.votes + 1
-      }
-      return state.map(a => a.id !== id ? a : updatedAnecdote)
-    }
-
-*/
 
 export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
